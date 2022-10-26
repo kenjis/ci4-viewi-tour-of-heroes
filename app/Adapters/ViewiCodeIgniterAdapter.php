76d1c2737e78431ec2342248fd45a3afb1967b63
kenjis <kenji.uui@gmail.com>
2022-10-26 16:59:26 +0900

    chore: update Adapters

--- app/Adapters/ViewiCodeIgniterAdapter.php
+++ app/Adapters/ViewiCodeIgniterAdapter.php
@@ -14,55 +14,26 @@ class ViewiCodeIgniterAdapter extends RouteAdapterBase
 {
     private CodeIgniter $app;
     private array $nameTracker = [];
+    private RouteSyntaxConverter $converter;
 
-    public function __construct(CodeIgniter $app)
+    public function __construct(CodeIgniter $app, RouteSyntaxConverter $converter)
     {
-        $this->app = $app;
+        $this->app       = $app;
+        $this->converter = $converter;
     }
 
+    /**
+     * @param string     $method
+     * @param string     $url
+     * @param string     $component
+     * @param array|null $defaults
+     */
     public function register($method, $url, $component, $defaults): void
     {
         /** @var RouteCollection $routes */
         $routes = Services::routes();
 
-        // Viewi routes: /, *, {userId}, {userId}, {name?}, {query<[A-Za-z]+>?}
-        // replace route params `{name}` with placeholders.
-        // {name} {name?} -> (:segment)
-        // * -> (:any)
-        $ciUrl = '';
-        $parts = explode(
-            '/',
-            str_replace('*', '(:any)', trim($url, '/'))
-        );
-        $paramNames = [];
-
-        foreach ($parts as $segment) {
-            if ($segment !== '' && $segment[0] === '{') {
-                $strLen    = strlen($segment) - 1;
-                $regOffset = -2;
-                $regex     = null;
-
-                if ($segment[$strLen - 1] === '?') { // {optional?}
-                    $strLen--;
-                    $regOffset = -3;
-                }
-
-                if ($segment[$strLen - 1] === '>') { // {<regex>}
-                    $strLen--;
-                    $regParts = explode('<', $segment);
-                    $segment  = $regParts[0];
-                    // {<regex>} -> ([a-z]+), (\d+)
-                    $regex = substr($regParts[1], 0, $regOffset);
-                    $regex = '(' . $regex . ')';
-                }
-
-                $paramName    = substr($segment, 1, $strLen - 1);
-                $paramNames[] = $paramName;
-                $segment      = $regex ?? '(:segment)';
-            }
-
-            $ciUrl .= '/' . $segment;
-        }
+        [$ciUrl, $paramNames] = $this->converter->convert($url);
 
         if (! isset($this->nameTracker[$component])) {
             $this->nameTracker[$component] = -1;
@@ -72,8 +43,8 @@ class ViewiCodeIgniterAdapter extends RouteAdapterBase
         $as = ($this->nameTracker[$component] === 0) ? $component
             : "{$component}-{$this->nameTracker[$component]}";
 
-        $routes->{$method}($ciUrl, static function (...$params) use ($component, $paramNames) {
-            $controller = new ViewiCodeIgniterComponent($component);
+        $routes->{$method}($ciUrl, static function (...$params) use ($component, $paramNames, $defaults) {
+            $controller = new ViewiCodeIgniterComponent($component, $defaults);
             // collect params
             $viewiParams = [];
 
@@ -87,6 +58,13 @@ class ViewiCodeIgniterAdapter extends RouteAdapterBase
         }, ['as' => $as]);
     }
 
+    /**
+     * @param string     $method
+     * @param string     $url
+     * @param array|null $params
+     *
+     * @return array|mixed|object
+     */
     public function handle($method, $url, $params = null)
     {
         /** @var CodeIgniter $app */
